3c8a3f9e7c2bd89ff03d7f7e97d987bc02d7d37b
Oliver Szabo <oleewere@gmail.com>
2019-08-18 15:28:11 +0200

    Add cli skeleton & dependencies

---
+++ cli/application.go
@@ -0,0 +1,34 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+// StartApplication holder of cli commands, can start cli application
+func StartApplication(version string, gitRevString string) {
+	app := cli.NewApp()
+	app.Name = "s3logs-zgrep"
+	app.Usage = "Tool for download and grep in s3 zipped logs"
+	app.EnableBashCompletion = true
+	app.UsageText = "s3logs-zgrep command [command options] [arguments...]"
+	if len(version) > 0 {
+		app.Version = version
+	} else {
+		app.Version = "0.1.0"
+	}
+	if len(gitRevString) > 0 {
+		app.Version = app.Version + fmt.Sprintf(" (git short hash: %v)", gitRevString)
+	}
+	app.Email = "oleewere@gmail.com"
+	app.Author = "Oliver Mihaly Szabo"
+	app.Copyright = "Copyright 2019 Oliver Mihaly Szabo"
+	app.Commands = []cli.Command{}
+	err := app.Run(os.Args)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/oleewere/s3logs-zgrep
+
+go 1.12
+
+require github.com/urfave/cli v1.21.0
